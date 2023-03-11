146e9d401cd25516d9a9dab11c2cbb22a3dcfb8f
StoianEmanuel <emanuel.stoian@student.unitbv.ro>
2023-03-11 14:29:48 +0200

    working two tables in a database + snippet

--- video games.py
+++ video games.py
@@ -6,9 +6,17 @@ from json import dumps
 app = Flask(__name__)
 
 app.config['SQLITE_DB_DIR'] = 'DB'
-app.config['SQLITE_DB_NAME'] = 'VideoGames.db'
+app.config['SQLITE_DB_NAME'] = 'gaming.db'
 
-# Retrieve all videoGames from the database
+# Function to establish connection with the database
+
+
+def get_db_connection():
+    conn = sqlite3.connect('DB\gaming.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+# Retrieve all games from the database
 
 
 def get_all_games(limit=None):
@@ -16,9 +24,9 @@ def get_all_games(limit=None):
         f"{app.config['SQLITE_DB_DIR']}/{app.config['SQLITE_DB_NAME']}")
     cursor = conn.cursor()
     if limit is None:
-        cursor.execute("SELECT * FROM VideoGames")
+        cursor.execute("SELECT * FROM videogames")
     else:
-        cursor.execute("SELECT * FROM VideoGames LIMIT ?", (limit,))
+        cursor.execute("SELECT * FROM videogames LIMIT ?", (limit,))
     rows = cursor.fetchall()
     games = []
     for row in rows:
@@ -46,58 +54,138 @@ def get_all_games(limit=None):
     return games
 
 
-# Function to establish connection with the database
-def get_db_connection():
-    conn = sqlite3.connect('DB\VideoGames.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
-# Define the route to retrieve all games
+# Retrieve all consoles from the database
 
 
-@app.route('/get_data', methods=['GET'])
+def get_all_consoles(limit=None):
+    conn = sqlite3.connect(
+        f"{app.config['SQLITE_DB_DIR']}/{app.config['SQLITE_DB_NAME']}")
+    cursor = conn.cursor()
+    if limit is None:
+        cursor.execute("SELECT * FROM consoles")
+    else:
+        cursor.execute("SELECT * FROM consoles LIMIT ?", (limit,))
+    rows = cursor.fetchall()
+    consoles = []
+    for row in rows:
+        console = {
+            '@id': row[0],
+            'name': row[1],
+            'manufacturer': row[2],
+            'year_of_release': row[3],
+            'sales': row[4],
+            'type': row[5],
+            'number_of_exclusives': row[6]
+        }
+        consoles.append(console)
+    conn.close()
+    return consoles
+
+# Define the route to retrieve all games/consoles
+
+
+@app.route('/sqlite/gaming/get_data', methods=['GET'])
 def get_data():
-    # Check if data_type is set to games
+    # Check if data_type is set to either games or consoles
     data_type = request.args.get('data_type')
-    if data_type != 'games':
+    snippet = request.args.get('snippet')
+    if data_type == 'consoles':
+        # Check if snippet parameter is provided
+        if snippet is None:
+            # Return error response if snippet parameter is missing
+            error_response = {
+                'error': 'snippet parameter is missing'
+            }
+            return jsonify(error_response), 400
+
+        # Retrieve consoles based on the value of snippet parameter
+        if snippet.lower() != 'true' and snippet.lower() != 'false':
+            error_response = {
+                'error': 'snippet parameter does not exists'
+            }
+            return jsonify(error_response), 404
+        if snippet.lower() == 'true':
+            # Retrieve first 10 consoles
+            consoles = get_all_consoles(limit=10)
+        elif snippet.lower() == 'false':
+            # Retrieve all consoles
+            consoles = get_all_consoles()
+
+        # Create JSON-LD document
+        context = {
+            "@schema": "sqlite"
+        }
+        data = {
+            "@context": context,
+            "@type": "consoles",
+            "@list": consoles
+        }
+        json_ld_doc = json.dumps(data)
+
+        # Return response in JSON-LD format
+        response = app.response_class(
+            response=json_ld_doc,
+            status=200,
+            mimetype='application/ld+json'
+        )
+        return response
+
+    elif data_type == 'games':
+        # Check if snippet parameter is provided
+        if snippet is None:
+            # Return error response if snippet parameter is missing
+            error_response = {
+                'error': 'snippet parameter is missing'
+            }
+            return jsonify(error_response), 400
+        # Retrieve games based on the value of snippet parameter
+        if snippet.lower() != 'true' and snippet.lower() != 'false':
+            error_response = {
+                'error': 'snippet parameter does not exists'
+            }
+            return jsonify(error_response), 400
+        if snippet.lower() == 'true':
+            # Retrieve first 10 games
+            games = get_all_games(limit=10)
+        elif snippet.lower() == 'false':
+            # Retrieve all games
+            games = get_all_games()
+
+        # Create JSON-LD document
+        context = {
+            "@schema": "sqlite"
+        }
+        data = {
+            "@context": context,
+            "@type": "video games",
+            "@list": games
+        }
+        json_ld_doc = json.dumps(data)
+
+        # Return response in JSON-LD format
+        response = app.response_class(
+            response=json_ld_doc,
+            status=200,
+            mimetype='application/ld+json'
+        )
+        return response
+
+    else:
         # Return error response if data_type is not games
         error_response = {
             'error': 'Invalid data type'
         }
         return jsonify(error_response), 400
 
-    # Retrieve up to 100 games
-    # limit = min(request.args.get('limit', default=10, type=int), 10)
-    # games = get_all_games(limit=limit)
 
-    # Retrieve all games
-    games = get_all_games()
-
-    # Create JSON-LD document
-    context = {
-        "@schema": "sqlite"
-    }
-    data = {
-        "@context": context,
-        "@type": "games",
-        "@list": games
-    }
-    json_ld_doc = json.dumps(data)
-
-    # Return response in JSON-LD format
-    response = app.response_class(
-        response=json_ld_doc,
-        status=200,
-        mimetype='application/ld+json'
-    )
-    return response
+# Retrieve a game from the database by id
 
 
 def get_game_by_id(id):
     conn = sqlite3.connect(
         f"{app.config['SQLITE_DB_DIR']}/{app.config['SQLITE_DB_NAME']}")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM VideoGames WHERE id=?", (id,))
+    cursor.execute("SELECT * FROM videogames WHERE id=?", (id,))
     row = cursor.fetchone()
     if row:
         game = {
@@ -127,7 +215,7 @@ def get_game_by_id(id):
 # Define the route to retrieve game by id
 
 
-@app.route('/games/get_data', methods=['GET'])
+@app.route('/sqlite/video_games/get_data_by_id', methods=['GET'])
 def get_game_data():
     # Check if the id is provided in the query string
     id = request.args.get('id')
@@ -154,8 +242,75 @@ def get_game_data():
     }
     data = {
         "@context": context,
-        "@type": "games",
+        "@type": "video games",
         "@list": [game]
+    }
+    json_ld_doc = json.dumps(data)
+
+    # Return response in JSON-LD format
+    response = app.response_class(
+        response=json_ld_doc,
+        status=200,
+        mimetype='application/ld+json'
+    )
+    return response
+
+# Retrieve a console from the database by id
+
+
+def get_console_by_id(id):
+    conn = sqlite3.connect(
+        f"{app.config['SQLITE_DB_DIR']}/{app.config['SQLITE_DB_NAME']}")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM consoles WHERE id=?", (id,))
+    row = cursor.fetchone()
+    if row:
+        console = {
+            '@id': row[0],
+            'name': row[1],
+            'manufacturer': row[2],
+            'year_of_release': row[3],
+            'sales': row[4],
+            'type': row[5],
+            'number_of_exclusives': row[6]
+        }
+    else:
+        console = None
+    conn.close()
+    return console
+
+# Define the route to retrieve console by id
+
+
+@app.route('/sqlite/consoles/get_data_by_id', methods=['GET'])
+def get_console_data():
+    # Check if the id is provided in the query string
+    id = request.args.get('id')
+    if id is None:
+        # Return error response if id is not provided
+        error_response = {
+            'error': 'id parameter is missing'
+        }
+        return jsonify(error_response), 400
+
+    # Retrieve game by id
+    console = get_console_by_id(id)
+
+    if console is None:
+        # Return error response if console is not found
+        error_response = {
+            'error': 'Console not found'
+        }
+        return jsonify(error_response), 404
+
+    # Create JSON-LD document
+    context = {
+        "@schema": "sqlite"
+    }
+    data = {
+        "@context": context,
+        "@type": "consoles",
+        "@list": [console]
     }
     json_ld_doc = json.dumps(data)
 
