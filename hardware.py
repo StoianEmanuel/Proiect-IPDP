4457d48d3aed6735a1473ea6069d1a4ee8bfd559
StoianEmanuel <emanuel.stoian@student.unitbv.ro>
2023-03-15 14:39:40 +0200

    major changes to db and removed some stuff

--- hardware.py
+++ hardware.py
@@ -18,7 +18,6 @@ def get_db_connection():
 
 # Retrieve all games from the database
 
-
 def get_all_games(limit=None):
     conn = sqlite3.connect(
         f"{app.config['SQLITE_DB_DIR']}/{app.config['SQLITE_DB_NAME']}")
@@ -38,8 +37,9 @@ def get_all_games(limit=None):
                 if row[i].upper() == 'NULL' or row[i] == '':
                     count += 1
             elif i == 3 and row[3].upper() != 'N/A':
-                if int(row[3]) < 1970:
-                    count += 1
+                if row[3].isnumeric():
+                    if int(row[3]) < 1970:
+                        count += 1
             elif i == 4 and int(row[4]) == 0:
                 count += 1
         game = {
@@ -112,6 +112,54 @@ def get_all_consoles(limit=None):
                 l += 1
     conn.close()
     return consoles
+
+#retrieve all mouses from database
+def get_all_mouses(limit=None):
+    conn = sqlite3.connect(
+        f"{app.config['SQLITE_DB_DIR']}/{app.config['SQLITE_DB_NAME']}")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM mouses")
+    # if limit is None:
+    #   cursor.execute("SELECT * FROM mouses")
+    # else:
+    #   cursor.execute("SELECT * FROM mouses LIMIT ?", (limit,))
+    rows = cursor.fetchall()
+    mouses = []
+    l = 0
+    for row in rows:
+        count = 0
+        for i in range(0, len(row)):
+            if i == 1 or i == 2 or i == 4 or i == 3 or i == 6 or i == 8 or i == 10:
+                if row[i].upper() == 'NULL' or row[i] == '':
+                    count += 1
+            elif i == 5 and (int(row[5]) < 3 or int(row[5]) > 20):
+                    count += 1
+            elif i == 7 and int(row[7]) > 250:
+                    count += 1
+            elif i == 9 and float(row[9]) > 5:
+                    count += 1
+        mouse = {
+            '@id': row[0],
+            'manufacturer': row[1],
+            'series': row[2],
+            'resolution': row[3],
+            'design': row[4],
+            'number of buttons': row[5],
+            'interface': row[6],
+            'weight': row[7],
+            'size': row[8],
+            'rating': row[9],
+            'link address': row[10]
+        }
+        if count < 4:
+            if limit is None:
+                mouses.append(mouse)
+            elif l < int(limit):
+                mouses.append(mouse)
+                l += 1
+    conn.close()
+    return mouses
+
 
 # Define the route to retrieve all games/consoles
 
@@ -201,159 +249,52 @@ def get_data():
             mimetype='application/ld+json'
         )
         return response
+    
+    elif data_type == 'mouses':
+        # Check if snippet parameter is provided
+        if snippet is None:
+            error_response = {
+                'error': 'snippet parameter is missing'
+            }
+            return jsonify(error_response), 400
+        # Retrieve mouses based on the value of snippet parameter
+        if snippet.lower() != 'true' and snippet.lower() != 'false':
+            error_response = {
+                'error': 'snippet parameter does not exists'
+            }
+            return jsonify(error_response), 400
+        if snippet.lower() == 'true':
+            # Retrieve first 10 mouses
+            mouses = get_all_mouses(limit=10)
+        elif snippet.lower() == 'false':
+            # Retrieve all mouses
+            mouses = get_all_mouses()
 
+        # Create JSON-LD document
+        context = {
+            "@schema": "sqlite/games"
+        }
+        data = {
+            "@context": context,
+            # "@type": "mouses",
+            "@list": mouses
+        }
+        json_ld_doc = json.dumps(data)
+
+        # Return response in JSON-LD format
+        response = app.response_class(
+            response=json_ld_doc,
+            status=200,
+            mimetype='application/ld+json'
+        )
+        return response
+    
     else:
-        # Return error response if data_type is not games or consoles
+        # Return error response if data_type is not games or consoles or mouses
         error_response = {
             'error': 'Invalid data type'
         }
         return jsonify(error_response), 400
-
-
-"""
-# Retrieve a game from the database by id
-
-
-def get_game_by_id(id):
-    conn = sqlite3.connect(
-        f"{app.config['SQLITE_DB_DIR']}/{app.config['SQLITE_DB_NAME']}")
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM videogames WHERE id=?", (id,))
-    row = cursor.fetchone()
-    if row:
-        game = {
-            '@id': row[0],
-            'name': row[1],
-            'platform': row[2],
-            'year_of_release': row[3],
-            'genre': row[4],
-            'publisher': row[5],
-            'na_sales': row[6],
-            'eu_sales': row[7],
-            'jp_sales': row[8],
-            'other_sales': row[9],
-            'global_sales': row[10],
-            'critic_score': row[11],
-            'critic_count': row[12],
-            'user_score': row[13],
-            'user_count': row[14],
-            'developer': row[15],
-            'rating': row[16]
-        }
-    else:
-        game = None
-    conn.close()
-    return game
-
-# Define the route to retrieve game by id
-
-
-@app.route('/sqlite/video_games/get_data_by_id', methods=['GET'])
-def get_game_data():
-    # Check if the id is provided in the query string
-    id = request.args.get('id')
-    if id is None:
-        # Return error response if id is not provided
-        error_response = {
-            'error': 'id parameter is missing'
-        }
-        return jsonify(error_response), 400
-
-    # Retrieve game by id
-    game = get_game_by_id(id)
-
-    if game is None:
-        # Return error response if game is not found
-        error_response = {
-            'error': 'Game not found'
-        }
-        return jsonify(error_response), 404
-
-    # Create JSON-LD document
-    context = {
-        "@schema": "sqlite"
-    }
-    data = {
-        "@context": context,
-        "@type": "video games",
-        "@list": [game]
-    }
-    json_ld_doc = json.dumps(data)
-
-    # Return response in JSON-LD format
-    response = app.response_class(
-        response=json_ld_doc,
-        status=200,
-        mimetype='application/ld+json'
-    )
-    return response
-
-# Retrieve a console from the database by id
-
-
-def get_console_by_id(id):
-    conn = sqlite3.connect(
-        f"{app.config['SQLITE_DB_DIR']}/{app.config['SQLITE_DB_NAME']}")
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM consoles WHERE id=?", (id,))
-    row = cursor.fetchone()
-    if row:
-        console = {
-            '@id': row[0],
-            'name': row[1],
-            'manufacturer': row[2],
-            'year_of_release': row[3],
-            'sales': row[4],
-            'type': row[5],
-            'number_of_exclusives': row[6]
-        }
-    else:
-        console = None
-    conn.close()
-    return console
-
-# Define the route to retrieve console by id
-
-
-@app.route('/sqlite/consoles/get_data_by_id', methods=['GET'])
-def get_console_data():
-    # Check if the id is provided in the query string
-    id = request.args.get('id')
-    if id is None:
-        # Return error response if id is not provided
-        error_response = {
-            'error': 'id parameter is missing'
-        }
-        return jsonify(error_response), 400
-
-    # Retrieve game by id
-    console = get_console_by_id(id)
-
-    if console is None:
-        # Return error response if console is not found
-        error_response = {
-            'error': 'Console not found'
-        }
-        return jsonify(error_response), 404
-
-    # Create JSON-LD document
-    context = {
-        "@schema": "sqlite"
-    }
-    data = {
-        "@context": context,
-        "@type": "consoles",
-        "@list": [console]
-    }
-    json_ld_doc = json.dumps(data)
-
-    # Return response in JSON-LD format
-    response = app.response_class(
-        response=json_ld_doc,
-        status=200,
-        mimetype='application/ld+json'
-    )
-    return response """
 
 
 if __name__ == '__main__':
